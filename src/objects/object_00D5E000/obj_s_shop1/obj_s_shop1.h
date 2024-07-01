ef5b4472809a90a3d5c76b52f9dbe1a77f017238
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-07-01 12:42:43 -0700

    s_shop1

---
+++ src/objects/object_00D5E000/obj_s_shop1/obj_s_shop1.h
@@ -0,0 +1,31 @@
+#ifndef OBJECT_OBJ_S_SHOP1_H
+#define OBJECT_OBJ_S_SHOP1_H
+
+#include "gbi.h"
+#include "c_keyframe.h"
+
+/**
+ * @file obj_s_shop1
+ * Nook's Cranny (non winter)
+ */
+
+extern Vtx obj_s_shop1_v[];
+extern Gfx obj_s_shop1_window_model[];
+extern Gfx obj_s_shop1_light_model[];
+extern Gfx obj_s_shop1_side_model[];
+extern Gfx obj_s_shop1_roof_model[];
+extern Gfx obj_s_shop1_front_model[];
+extern Gfx obj_s_shop1_door_model[];
+extern u8 obj_s_shop1_front_txt[];
+extern u8 obj_s_shop1_roof_txt[];
+extern u8 obj_s_shop1_side_txt[];
+extern u8 obj_s_shop1_window_txt[];
+extern JointElemR cKF_je_r_obj_s_shop1_tbl[];
+extern BaseSkeletonR cKF_bs_r_obj_s_shop1;
+extern u8 cKF_ckcb_r_obj_s_shop1_tbl[];
+extern s16 cKF_kn_obj_s_shop1_tbl[];
+extern s16 cKF_c_obj_s_shop1_tbl[];
+extern Keyframe cKF_ds_obj_s_shop1_tbl[];
+extern BaseAnimationR cKF_ba_r_obj_s_shop1;
+
+#endif
