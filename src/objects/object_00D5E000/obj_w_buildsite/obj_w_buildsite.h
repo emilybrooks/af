d25e9ab2f36073c6724cee2a79963e4a79658c2e
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-07-01 11:46:50 -0700

    w_buildsite

---
+++ src/objects/object_00D5E000/obj_w_buildsite/obj_w_buildsite.h
@@ -0,0 +1,10 @@
+#ifndef OBJECT_OBJ_W_BUILDSITE_H
+#define OBJECT_OBJ_W_BUILDSITE_H
+
+#include "gbi.h"
+
+extern Vtx obj_w_buildsite_v[];
+extern Gfx reserve_winter_DL_model[];
+extern u8 obj_w_buildsite_tex[];
+
+#endif
