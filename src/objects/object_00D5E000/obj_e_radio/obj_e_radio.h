ff5efa045ebf78fbd0222aa915b239c718495bc1
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-07-01 10:50:35 -0700

    radio

---
+++ src/objects/object_00D5E000/obj_e_radio/obj_e_radio.h
@@ -0,0 +1,13 @@
+#ifndef OBJECT_OBJ_E_RADIO_H
+#define OBJECT_OBJ_E_RADIO_H
+
+#include "gbi.h"
+
+extern Vtx obj_e_radio_v[];
+extern Gfx obj_e_radio_t1_model[];
+extern Gfx obj_e_radio_t2T_model[];
+extern Gfx radio_DL_model[];
+extern u8 obj_e_radio_t1_tex[];
+extern u8 obj_e_radio_t2_tex[];
+
+#endif
