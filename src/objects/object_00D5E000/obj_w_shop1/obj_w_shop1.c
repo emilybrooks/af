70d41cc939bc16cbcd1374b9d255f7b3c77f352a
emilybrooks <emilybrooksemilybrooks@gmail.com>
2024-07-01 19:31:45 -0700

    w_shop1

---
+++ src/objects/object_00D5E000/obj_w_shop1/obj_w_shop1.c
@@ -0,0 +1,29 @@
+#include "obj_w_shop1.h"
+
+s32 padDB8430[] = { 0x00000000, 0x00000000 };
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/obj_w_shop1_v.vtx.inc.c"
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/obj_w_shop1_window_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/obj_w_shop1_light_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/obj_w_shop1_side_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/obj_w_shop1_roof_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/obj_w_shop1_front_model.gfx.inc.c"
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/obj_w_shop1_door_model.gfx.inc.c"
+u8 obj_w_shop1_front_txt[] = {
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/obj_w_shop1_front_txt.ci4.inc.c"
+};
+u8 obj_w_shop1_roof_txt[] = {
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/obj_w_shop1_roof_txt.ci4.inc.c"
+};
+u8 obj_w_shop1_side_txt[] = {
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/obj_w_shop1_side_txt.ci4.inc.c"
+};
+u8 obj_w_shop1_window_txt[] = {
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/obj_w_shop1_window_txt.i4.inc.c"
+};
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/cKF_je_r_obj_w_shop1_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/cKF_bs_r_obj_w_shop1.inc.c"
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/cKF_ckcb_r_obj_w_shop1_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/cKF_kn_obj_w_shop1_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/cKF_c_obj_w_shop1_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/cKF_ds_obj_w_shop1_tbl.inc.c"
+#include "assets/jp/objects/object_00D5E000//obj_w_shop1/cKF_ba_r_obj_w_shop1.inc.c"
